8d550fcdc4283c03dc1c5513710f053edbdbf795
brking014 <281108211@163.com>
2018-09-21 14:15:39 +0800

    change deepT

--- tensorflowLearn/deepTensorflow.py
+++ tensorflowLearn/deepTensorflow.py
@@ -17,10 +17,13 @@ def conv2d(x, W):
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(
+        x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-mnist = input_data.read_data_sets("D:\\Project_Work\\MachineLearning\\PythonWorkSpace\\MNIST_data", one_hot=True)
+mnist = input_data.read_data_sets(
+    "../MNIST_data",
+    one_hot=True)
 sess = tf.InteractiveSession()
 x = tf.placeholder("float", shape=[None, 784])
 y_ = tf.placeholder("float", shape=[None, 10])
@@ -49,7 +52,7 @@ h_pool2 = max_pool_2x2(h_conv2)
 
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder("float")
@@ -60,16 +63,24 @@ b_fc2 = bias_variable([10])
 
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.initialize_all_variables())
 for i in range(20000):
     batch = mnist.train.next_batch(50)
-    if i % 1 == 0:
-        train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+    if i % 100 == 0:
+        train_accuracy = accuracy.eval(feed_dict={
+            x: batch[0],
+            y_: batch[1],
+            keep_prob: 1.0
+        })
         print("step %d, training accuracy %g" % (i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print("test accuracy %g" % accuracy.eval(feed_dict={
+    x: mnist.test.images,
+    y_: mnist.test.labels,
+    keep_prob: 1.0
+}))
--- tensorflowLearn/mnsi.py
+++ tensorflowLearn/mnsi.py
@@ -1,8 +1,9 @@
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 import tensorflow as tf
 
-
-mnist = input_data.read_data_sets("D:\\Project_Work\\MachineLearning\\PythonWorkSpace\\MNIST_data", one_hot=True)
+mnist = input_data.read_data_sets(
+    "D:\\Project_Work\\MachineLearning\\PythonWorkSpace\\MNIST_data",
+    one_hot=True)
 # 我们通过操作符号变量来描述这些可交互的操作单元，可以用下面的方式创建一个：
 x = tf.placeholder(tf.float32, [None, 784])
 # 我们的模型也需要权重值和偏置量，当然我们可以把它们当做是另外的输入（使用占位符），但TensorFlow有一个更好的方法来表示它们：Variable 。 一个Variable代表一个可修改的张量，存在在TensorFlow的用于描述交互性操作的图中。它们可以用于计算输入值，也可以在计算中被修改。对于各种机器学习应用，一般都会有模型参数，可以用Variable表示。
@@ -12,7 +13,7 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 # 计算交叉熵:
 y_ = tf.placeholder("float", [None, 10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 # 现在我们知道我们需要我们的模型做什么啦，用TensorFlow来训练它是非常容易的。因为TensorFlow拥有一张描述你各个计算单元的图，它可以自动地使用反向传播算法(backpropagation algorithm)来有效地确定你的变量是如何影响你想要最小化的那个成本值的。然后，TensorFlow会用你选择的优化算法来不断地修改变量以降低成本。
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 # 我们需要添加一个操作来初始化我们创建的变量
@@ -29,4 +30,9 @@ for i in range(1000):
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 # 这行代码会给我们一组布尔值。为了确定正确预测项的比例，我们可以把布尔值转换成浮点数，然后取平均值。例如，[True, False, True, True] 会变成 [1,0,1,1] ，取平均值后得到 0.75.
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(
+    sess.run(
+        accuracy, feed_dict={
+            x: mnist.test.images,
+            y_: mnist.test.labels
+        }))
